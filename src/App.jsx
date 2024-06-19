a5def60729f50afbe68a9564641c3c3b168b5fa9
pr-askme <hi.priya.workspace@gmail.com>
2024-06-19 21:37:49 +0530

    Add: Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import HomePage from './pages/HomePage'
-import PageNotfound from './pages/PageNotfound'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotfound from "./pages/PageNotfound";
+import "./index.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="product" element={<Product/>} />
-      <Route path="pricing" element={<Pricing/>} />
-      <Route path="*" element={<PageNotfound/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotfound />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-
