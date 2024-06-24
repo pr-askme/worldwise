20b82c0c1b2b2b18b2237f0851714b2d5652e4c2
pr-askme <hi.priya.workspace@gmail.com>
2024-06-24 21:08:01 +0530

    Add:Finishing the City page view

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,14 +9,15 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 
-export default function CityItem({ city }) {
+export default function CityItem({city}) {
   const { cityName, emoji, date, id, position } = city;
+  const {currentCity} = useCities();
 
   return (
     <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${id===currentCity.id ? styles['cityItem--active'] : ""}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
